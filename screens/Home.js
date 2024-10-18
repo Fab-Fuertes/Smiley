d527a4bb57a6259e7cd02a68740da2576394f3fa
aemr-LL7 <a.marquez2020@correo.unimet.edu.ve>
2024-10-18 18:02:02 -0400

    [+] Testing with API Server

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,13 @@
 import { Text, StyleSheet, View } from 'react-native'
 import React, { useState, useEffect } from 'react'
+import DataDisplay from './DataDisplay'
 
 export default function Inicio() {
 
     return (
       <View>
         <Text>Bienvenido</Text>
+        <DataDisplay />
       </View>
     )
 }
