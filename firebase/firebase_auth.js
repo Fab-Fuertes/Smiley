9bc7c12ac9a49327426fdcb4e562d3eb3afdf424
aemr-LL7 <a.marquez2020@correo.unimet.edu.ve>
2024-11-07 20:00:04 -0400

    [//] Configuring WebServer

---
+++ firebase/firebase_auth.js
@@ -0,0 +1,261 @@
+const { Functions } = require('firebase-admin/functions');
+const { auth, database } = require('./databaseConfig'); // Cambia esto a firebase-admin.js
+const { collection, doc, setDoc } = require('firebase-admin/firestore');
+
+async function logIn(tokenID) {
+  try {
+
+    const userRecordValidate = tokenID._tokenResponse.idToken;
+    // Verificar el ID token del cliente
+    const decodedToken = await auth.verifyIdToken(userRecordValidate);
+    const uid = decodedToken.uid;
+
+    // Generar un token personalizado para el usuario si es necesario
+    const customToken = await auth.createCustomToken(uid);
+    console.log('Token personalizado:', customToken);
+    const id = tokenID.user.uid
+    const docRef = database.collection("users").doc(id);
+    const docSnap = await docRef.get();
+    console.log(docSnap.data())
+    return docSnap.data();
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error.message);
+    return null;
+  }
+  
+
+}
+// async function loginUser(email, password) {
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(auth, email, password);
+//     const idToken = await userCredential.user.getIdToken();
+//     return idToken; // Enviar este token al servidor
+//   } catch (error) {
+//     console.error("Error al iniciar sesión:", error);
+//     throw error;
+//   }
+// }
+
+
+async function addVolun(data) {
+  try {
+    const { userId, fundacionId, name, last_name } = data;
+    
+    // Check if all required fields are present
+    if (!userId || !fundacionId || !name || !last_name) {
+      throw new Error('Missing required fields');
+    }
+
+    const fundacionRef = database.collection("voluntariado").doc(fundacionId);
+
+    // Use a transaction to ensure data consistency
+    return await database.runTransaction(async (transaction) => {
+      const fundacionDoc = await transaction.get(fundacionRef);
+
+      if (!fundacionDoc.exists) {
+        // If the document doesn't exist, create it with the first volunteer
+        transaction.set(fundacionRef, {
+          volunteers: [{
+            userId,
+            name,
+            last_name,
+          }]
+        });
+      } else {
+        const volunteers = fundacionDoc.data().volunteers || [];
+        const existingVolunteer = volunteers.find(volunteer => volunteer.userId === userId);
+
+        if (existingVolunteer) {
+          //console.log(El userId ${userId} ya está registrado como voluntario.);
+          return null;
+        } else {
+          // If the userId doesn't exist in the volunteers list, add it
+          const newVolunteer = {
+            userId,
+            name,
+            last_name,
+          };
+          
+          // Filter out any undefined values
+          Object.keys(newVolunteer).forEach(key => newVolunteer[key] === undefined && delete newVolunteer[key]);
+
+          const newVolunteers = [...volunteers, newVolunteer];
+          transaction.update(fundacionRef, { volunteers: newVolunteers });
+        }
+      }
+
+      return userId;
+    });
+  } catch (error) {
+    console.error("Error al registrar usuario:", error);
+    throw error;
+  }
+}
+
+async function getVoluntariados(id) {
+  try {
+    const usersRef = database.collection('voluntariado').doc(id);
+    const snapshot = await usersRef.get();
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }
+    console.log(snapshot.data())
+    const fund = snapshot.data();
+    return fund;
+  } catch (error) {
+    console.error('Error getting documents: ', error);
+    return null;
+  }
+}
+
+
+async function signUp(data) {
+  try {
+    const userRecordValidate = await emailVerification(data.email);
+    if(userRecordValidate != null){
+      console.log("El usuario ya existe:", userRecordValidate);
+      return null;
+    }
+    // Registra al usuario en Firebase Authentication
+    const userRecord = await auth.createUser({
+      email: data.email,
+      password: data.password,
+      displayName: `${data.name}` `${data.last_name}`
+    });
+    console.log(userRecord)
+    const userId = userRecord.uid;
+ 
+    let colection;
+   if (data.user==false){
+    colection= "fundaciones"
+    await database.collection(colection).doc(userId).set({
+      tittle: data.tittle,
+      type: data.type,
+      email: data.email,
+      description: data.description,
+      shortDescription: data.shortDescription,
+      location: data.location,
+
+    });
+   }
+    else{
+      colection= "users"
+         await database.collection(colection).doc(userId).set({
+      email: data.email,
+      name: data.name,
+      lastname: data.last_name,
+      phone: data.phone,
+      country: data.country,
+    });
+    }
+    // Agrega los datos del usuario a la colección "users" en Firestore
+ 
+
+    return userId;
+  } catch (error) {
+    console.error("Error al registrar usuario:", error);
+    return null;
+  }
+}
+
+async function emailVerification(email) {
+  try {
+    // Obtén el usuario por email
+
+    const userRecord = await auth.getUserByEmail(email);
+
+    // Genera el enlace de verificación de email
+
+    // Retorna el userId y el enlace de verificación
+    console.log("El usuario ya existe:", userRecord.uid);
+    return userRecord.uid
+  } catch (error) {
+    console.error("Error al verificar email:", error);
+    return null;
+  }
+}
+async function getFundaciones() {
+  try {
+    const usersRef = database.collection('fundaciones');
+    const snapshot = await usersRef.get();
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }
+    const fund = [];
+    snapshot.forEach(doc => {
+      console.log(doc.id)
+
+      fund.push({ id: doc.id, ...doc.data() });
+    });
+    return fund;
+  } catch (error) {
+    console.error('Error getting documents: ', error);
+    return null;
+  }
+}
+async function getImageUrl(userId) {
+  try {
+    // Obtiene una lista de todos los archivos en la carpeta images/userId
+    const listRef = ref(storage, images/`${userId}`/ac91c797b888cc696cde48c2ef8bcfe6.jpg);
+    console.log(listRef)
+    // const listResult = await listAll(listRef);
+
+    // // Verifica que haya al menos un archivo
+    // if (listResult.items.length > 0) {
+    //   // Obtiene el primer archivo de la lista (asumiendo que solo hay uno)
+    //   const firstFile = listResult.items[0];
+
+    //   // Obtiene la URL de descarga del archivo
+    //   const imageUrl = await getDownloadURL(firstFile);
+
+    //   return imageUrl; // Devuelve la URL de descarga de la imagen
+    // } else {
+    //   console.error("No se encontraron archivos para el usuario:", userId);
+    //   return null; // Devuelve null si no se encontraron archivos
+    // }
+  } catch (error) {
+    console.error("Error al obtener la URL de la imagen:", error);
+    return null; // Devuelve null en caso de error
+  }
+}
+async function getFoundationType(type){
+  try {
+    const usersRef = database.collection('fundaciones');
+    const snapshot = await usersRef.where('type', '==', type).get();
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }
+    const fund = [];
+    snapshot.forEach(doc => {
+      console.log(doc.id)
+
+      fund.push({ id: doc.id, ...doc.data() });
+    });
+    return fund;
+  } catch (error) {
+    console.error('Error getting documents: ', error);
+    return null;
+  }
+
+}
+
+async function getFoundation(id){
+  try {
+    const usersRef = database.collection('fundaciones').doc(id);
+    const snapshot = await usersRef.get();
+    if (snapshot.empty) {
+      console.log('No matching documents.');
+      return;
+    }
+    console.log(snapshot.data())
+    const fund = snapshot.data();
+    return fund;
+  } catch (error) {
+    console.error('Error getting documents: ', error);
+    return null;
+  }
+}
+module.exports = { signUp, getFoundationType,logIn, getFundaciones ,getFoundation,addVolun,getVoluntariados};
--- serveur.js
+++ serveur.js
@@ -1,257 +1,82 @@
+require("dotenv").config();
+const { signUp, logIn } = require("./firebase/firebase_auth"); // Asegúrate de que la ruta a tu archivo de configuración de Firebase sea correcta
+const { admin } = require("./firebase/databaseConfig");
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-require("dotenv").config();
-const db = require("./firebaseConfig");
+const bodyParser = require("body-parser");
 
-// Acceso a la base de datos
+// Crear una instancia de la aplicación Express
 const app = express();
-const port = 8000;
-app.use(cors());
 
-// WebSocket para actualizaciones en tiempo real
-const WebSocket = require("ws");
-const http = require("http");
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+// Obtener el puerto desde las variables de entorno o usar el puerto 8000 como valor predeterminado
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json()); // Middleware para analizar JSON
+
+// Asignar puerto
+const PORT = process.env.PORT || 8001;
+
+//const path = require("path");
+
 
 // Ruta para obtener un mensaje simple
 app.get("/", async (req, res) => {
-  res.json("hiiii we working");
+  res.json("hiiii we working on another dimension");
 });
 
-// Ruta para obtener toda la base de datos desde Firebase
-app.get("/test", async (req, res) => {
+// Ruta para obtener todos los datos de la base de datos
+app.get('/data', async (req, res) => {
   try {
-    const ref = db.ref("/");
-    const snapshot = await ref.once("value");
+    const snapshot = await admin.database().ref('/').once('value');
     const data = snapshot.val();
     res.json(data);
   } catch (error) {
-    console.error("Error al obtener los datos de Firebase:", error);
-    res.status(500).json({ error: "Error al obtener los datos de Firebase" });
+    console.error('Error al obtener datos:', error);
+    res.status(500).send('Error al obtener datos');
   }
 });
-
-// Ruta para contar todas las opiniones
-app.get("/count-opinions", async (req, res) => {
-  try {
-    const ref = db.ref("/opiniones");
-    const snapshot = await ref.once("value");
-    const opinionsData = snapshot.val();
-
-    const opinionCounts = {};
-
-    for (const userId in opinionsData) {
-      const opinions = opinionsData[userId];
-
-      for (const timestamp in opinions) {
-        const opinion = opinions[timestamp];
-        const terminalId = opinion.id;
-
-        if (!opinionCounts[terminalId]) {
-          opinionCounts[terminalId] = {
-            Bien: 0,
-            Ok: 0,
-            Regular: 0,
-            Mal: 0,
-          };
-        }
-
-        opinionCounts[terminalId][opinion.apreciacion]++;
-      }
-    }
-
-    res.json(opinionCounts);
-  } catch (error) {
-    console.error("Error counting opinions:", error);
-    res.status(500).json({ error: "Error counting opinions" });
-  }
-});
-
-// Ruta para contar solo las apreciaciones positivas del rating ("Bien")
-app.get("/count-positive-opinions", async (req, res) => {
-  try {
-    const ref = db.ref("/opiniones");
-    const snapshot = await ref.once("value");
-    const opinionsData = snapshot.val();
-
-    const positiveCounts = [];
-
-    for (const terminalId in opinionsData) {
-      const opinions = opinionsData[terminalId];
-      let positiveCount = 0;
-
-      for (const timestamp in opinions) {
-        const opinion = opinions[timestamp];
-        if (opinion.apreciacion === "Muy Bien" || opinion.apreciacion === "Bien") {
-          positiveCount++;
-        }
-      }
-
-      positiveCounts.push({ terminalId, positiveCount });
-    }
-
-    // Ordenar los baños por la cantidad de apreciaciones "Bien"
-    positiveCounts.sort((a, b) => b.positiveCount - a.positiveCount);
-
-    res.json(positiveCounts);
-  } catch (error) {
-    console.error("Error counting positive opinions:", error);
-    res.status(500).json({ error: "Error counting positive opinions" });
-  }
-});
-
-// Monitoreo de las reacciones y envío de alertas si se superan los umbrales
-const monitorReactions = () => {
-  const terminalRef = db.ref("Terminales");
-
-  terminalRef.on("value", (snapshot) => {
-    const terminals = snapshot.val();
-
-    if (terminals) {
-      Object.entries(terminals).forEach(([terminalId, terminalData]) => {
-        const umbral = terminalData.umbral || 0;
-
-        const opinionsRef = db.ref(`opiniones/${terminalData.ID}`);
-        opinionsRef.on("value", (opinionsSnapshot) => {
-          const opinionsData = opinionsSnapshot.val() || {};
-          const totalOpinions = Object.keys(opinionsData).length;
-
-          let negativeReactions = 0;
-
-          Object.values(opinionsData).forEach((opinion) => {
-            if (
-              opinion.apreciacion === "Mal" ||
-              opinion.apreciacion === "Regular"
-            ) {
-              negativeReactions++;
-            }
-          });
-
-          const negativePercentage = (negativeReactions / totalOpinions) * 100;
-
-          if (negativePercentage > umbral) {
-            sendAlert(terminalId, negativeReactions, umbral);
-          }
-        });
-      });
-    }
-  });
-};
-
-const sendAlert = (terminalId, reaccionesNegativas, umbral) => {
-  const mensaje = `El terminal ${terminalId} ha superado el umbral de reacciones negativas (Neg: ${reaccionesNegativas}/ Umbral:${umbral}). Se requiere atención de mantenimiento.`;
-  console.log(mensaje);
-};
-
-// Iniciar el monitoreo de reacciones
-monitorReactions();
-
-// Ruta para actualizar el umbral
-app.post("/update-dynamic-threshold", async (req, res) => {
-  try {
-    const terminalRef = db.ref("Terminales");
-    const snapshot = await terminalRef.once("value");
-    const terminals = snapshot.val();
-
-    if (terminals) {
-      await Promise.all(
-        Object.entries(terminals).map(async ([terminalId, terminalData]) => {
-          const opinionsRef = db.ref(`opiniones/${terminalData.ID}`);
-          const opinionsSnapshot = await opinionsRef.once("value");
-          const opinionsData = opinionsSnapshot.val() || {};
-
-          const totalOpinions = Object.keys(opinionsData).length;
-          let negativeReactions = 0;
-
-          Object.values(opinionsData).forEach((opinion) => {
-            if (
-              opinion.apreciacion === "Mal" ||
-              opinion.apreciacion === "Regular"
-            ) {
-              negativeReactions++;
-            }
-          });
-
-          const negativePercentage =
-            totalOpinions > 0 ? (negativeReactions / totalOpinions) * 100 : 0;
-
-          await terminalRef
-            .child(`${terminalId}/umbral`)
-            .set(negativePercentage);
-        })
-      );
-    }
-
-    res
-      .status(200)
-      .json({ message: "Los umbrales se han actualizado correctamente!" });
-  } catch (error) {
-    console.error("Error actualizando umbrales:", error);
-    res.status(500).json({ error: "Error actualizando umbrales" });
-  }
-});
-
-// WebSocket para actualizaciones en tiempo real desde Firebase
-wss.on("connection", (ws) => {
-  console.log("Nuevo cliente WebSocket conectado");
-
-  // Enviar la información inicial de la base de datos
-  const terminalRef = db.ref("Terminales");
-  terminalRef.once("value", (snapshot) => {
-    const terminals = snapshot.val();
-    ws.send(JSON.stringify(terminals));
-  });
-
-  // Escuchar cambios en la base de datos y enviar actualizaciones
-  const onValueChange = (snapshot) => {
-    const terminals = snapshot.val();
-    ws.send(JSON.stringify(terminals));
-  };
-
-  terminalRef.on("value", onValueChange);
-
-  // Manejo de la desconexión
-  ws.on("close", () => {
-    console.log("Conexión WebSocket cerrada");
-    terminalRef.off("value", onValueChange); // Desactivar el listener al cerrar
-  });
-});
-
-// Funcion de prueba para simular una nueva opinion
-const simulateOpinion = async () => {
-  const terminalId = "00001"; // Cambia al ID correspondiente
-  const opinionsRef = db.ref(`opiniones/${terminalId}`);
-  
-  try {
-    const snapshot = await opinionsRef.once("value");
-    const existingOpinions = snapshot.val() || {};
-    const totalOpinions = Object.keys(existingOpinions).length;
-
-    // Si el total de opiniones es menor a un cierto límite, se permite agregar una nueva
-    if (totalOpinions < 100) { // Cambia 100 al límite deseado
-      const newOpinion = {
-        apreciacion: Math.random() > 0.5 ? "Bien" : "Mal", // Genera aleatoriamente "Bien" o "Mal"
-        fecha: new Date().toISOString().split("T")[0],
-        hora: new Date().toISOString().split("T")[1].split(".")[0],
-        id: terminalId,
-      };
-
-      await opinionsRef.push(newOpinion); // Agrega la nueva opinión a Firebase
-      console.log("Nueva opinión añadida:", newOpinion);
-    } else {
-      console.log(`Límite de opiniones alcanzado para el terminal ${terminalId}.`);
-    }
-  } catch (error) {
-    console.error("Error al simular opinión:", error);
-  }
-};
-
-
-// setInterval(simulateOpinion, 15000); // cada 15 segundos
 
 // Escuchar en el puerto
-server.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    console.log(req.body);
+    const userId = await logIn(req.body.tokenID); // Espera a que signUp resuelva la promesa
+    if (userId) {
+      res.status(200).send({
+        message: "Usuario",
+        userId: userId,
+        user: req.body.tokenID.user.uid,
+      });
+    } else {
+      res.status(400).send({ message: "Error al  usuario" });
+    }
+  } catch (error) {
+    console.error("Error al  usuario: ", error);
+  }
+});
+
+app.post("/signUp", async (req, res) => {
+  // console.log("entro")
+  // console.log()
+  // const array= req.body["Datos"];
+  try {
+    const userId = await signUp(req.body); // Espera a que signUp resuelva la promesa
+
+    if (userId) {
+      res
+        .status(200)
+        .send({ message: "Usuario registrado con éxito", userId: userId });
+    } else if (userId == null) {
+      res.status(400).send({ message: "Usuario ya registrado" });
+    } else {
+      res.status(400).send({ message: "Error al registrar usuario" });
+    }
+  } catch (error) {
+    console.error("Error al registrar usuario: ", error);
+    res.status(500).send("Error al registrar usuario");
+  }
 });
